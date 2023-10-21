b14206cc537745287615ed72507090b783913174
Valentin <valentinale203@gmail.com>
2023-10-21 10:41:13 -0300

    Add class Customer

---
+++ Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace PruebaTecnicaSofftek.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Account List<Account>();
+    }
+}
