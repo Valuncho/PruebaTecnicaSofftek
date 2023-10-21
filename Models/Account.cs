29b398247d32debef86a9d4f3f13b0c99a52de75
Valentin <valentinale203@gmail.com>
2023-10-21 12:10:18 -0300

    Added methods to Account class

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,8 +1,28 @@
-﻿namespace PruebaTecnicaSofftek.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace PruebaTecnicaSofftek.Models
 {
+    [Table("account")]
     public class Account
     {
         public int AccountId { get; set; }
-        public string Balance { get; set; }
+        public decimal Balance { get; set; }
+        public decimal GetBalance () { return Balance; }
+        public decimal Withdrawal(decimal AmmountToWithdraw) 
+        {
+            if (Balance > 0 && Balance >= AmmountToWithdraw) 
+            {
+                Balance -= AmmountToWithdraw;
+                return AmmountToWithdraw;
+            }
+            // Devuelve un valor diferente o lanza una excepción para indicar un retiro fallido.
+            // En este caso, estoy devolviendo -1 para indicar un retiro no exitoso.
+            return -1;
+        }
+        public void DepositMoney(decimal MoneyToDeposit) 
+        {
+            Balance += MoneyToDeposit;
+        }
     }
 }
