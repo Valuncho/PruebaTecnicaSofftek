9756d9139b36191afd5bf4ff1bfb8ca56abc162f
Valentin <valentinale203@gmail.com>
2023-10-21 10:46:12 -0300

    Add class Transfer

---
+++ Models/Transfer.cs
@@ -0,0 +1,9 @@
+﻿namespace PruebaTecnicaSofftek.Models
+{
+    public class Transfer
+    {
+        public Account Origin { get; set; }
+        public Account Destination { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
