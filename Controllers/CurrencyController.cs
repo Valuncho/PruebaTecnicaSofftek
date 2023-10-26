82d23f656ad74b7bac241aec8d03f18870e92791
Valentin <valentinale203@gmail.com>
2023-10-25 21:06:20 -0300

    Fixed the error to obtain the prices of the Dollar and Crypto

--- Controllers/CurrencyController.cs
+++ Controllers/CurrencyController.cs
@@ -24,7 +24,7 @@ namespace PruebaTecnicaSofftek.Controllers
         {
             try
             {
-                // Realiza una solicitud GET a la API de DolarAPI
+                /* Realiza una solicitud GET a la API de DolarAPI
                 HttpResponseMessage responseUSD = await _client.GetAsync("https://dolarapi.com/v1/dolares/blue");
                 HttpResponseMessage responseBTC = await _client.GetAsync("https://criptoya.com/api/banexcoin/btc/usd/0.1");
 
@@ -35,12 +35,37 @@ namespace PruebaTecnicaSofftek.Controllers
                     var dolarSplitted = USDData.Split(',');
                     var dolarValue = dolarSplitted[4];
                     decimal dolarPrice = 
-                    Console.Write(dolarPrice);
+                    //Console.Write(dolarPrice);
                     string BTCData = await responseBTC.Content.ReadAsStringAsync();
                     currencyInformation.SetDolarInformation(USDData);
                     currencyInformation.SetCryptoInformation(BTCData);
                     return Ok(USDData +"\n"+ BTCData);
+                }*/
+                HttpResponseMessage responseUSD = await _client.GetAsync("https://dolarapi.com/v1/dolares/blue");
+                HttpResponseMessage responseBTC = await _client.GetAsync("https://criptoya.com/api/banexcoin/btc/usd/0.1");
+
+                if (responseUSD.IsSuccessStatusCode && responseBTC.IsSuccessStatusCode)
+                {
+                    // Lee y procesa la respuesta como JSON
+                    string USDData = await responseUSD.Content.ReadAsStringAsync();
+                    string BTCData = await responseBTC.Content.ReadAsStringAsync();
+
+                    // Utiliza JsonDocument para analizar los datos JSON
+                    using (JsonDocument usdDocument = JsonDocument.Parse(USDData))
+                    using (JsonDocument btcDocument = JsonDocument.Parse(BTCData))
+                    {
+                        // Accede a los valores específicos en USD y BTC
+                        decimal dolarPrice = usdDocument.RootElement.GetProperty("venta").GetDecimal();
+                        decimal bitcoinPrice = btcDocument.RootElement.GetProperty("bid").GetDecimal();
+
+                        // Puedes utilizar dolarPrice y bitcoinPrice según tus necesidades
+                        currencyInformation.SetDolarInformation(USDData);
+                        currencyInformation.SetCryptoInformation(BTCData);
+
+                        return Ok($"Valor del dólar: {dolarPrice}\nValor de Bitcoin: {bitcoinPrice}");
+                    }
                 }
+
                 else
                 {
                     return BadRequest("Error en la solicitud a la API de DolarAPI.");
