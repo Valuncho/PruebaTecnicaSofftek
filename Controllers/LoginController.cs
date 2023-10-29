d4c0a93b77c4b6bb0226e1e63dc0042e77584ee1
Valentin <valentinale203@gmail.com>
2023-10-28 22:14:49 -0300

    Added comments to LoginController

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -16,29 +16,34 @@ namespace PruebaTecnicaSofftek.Controllers
         public LoginController(IUnitOfWork unitOfWork, IConfiguration configuration)
         {
             _unitOfWork = unitOfWork;
+            // instancia un nuevo token
             _tokenJwtHelper = new TokenJwtHelper(configuration);
         }
-
+        /// <summary>
+        /// Realiza la autenticacion de un usuario y genera un token de acceso.
+        /// </summary>
+        /// <param name="dto">Los datos de autenticación proporcionados por el usuario.</param>
+        /// <returns>Una respuesta HTTP que indica el resultado de la autenticación y en caso de exito, un token de acceso.</returns>
         [HttpPost]
         [ProducesResponseType(typeof(ApiSuccessResponse<LoginDto>), 200)]
         [ProducesResponseType(typeof(ApiErrorResponse), 401)]
         public async Task<IActionResult> Login(AutenticacionDto dto)
         {
+            // Realiza la autenticacion del usuario utilizando los datos proporcionados en el objeto dto
             var userCredentials = await _unitOfWork.CustomerRepository.AuthenticateCredentials(dto);
-
+            // Si la authenticacion no existe devuelve un Unauthorized
             if (userCredentials is null) return Unauthorized();
-
+            // Genera un token de acceso para el usuario autenticado
             var token = _tokenJwtHelper.GenerateToken(userCredentials);
-
+            // Crea un objeto LoginDto que contiene informacion sobre el usuario autenticado y el token generado.
             var user = new LoginDto()
             {
                 Email = userCredentials.Email,
                 Name = userCredentials.CustomerName,
                 Token = token
             };
-
+            // Devuelve un objeto user
             return ResponseFactory.CreateSuccessResponse(200, user);
-
         }
     }
 }
